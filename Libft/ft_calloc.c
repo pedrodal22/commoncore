c2d6d7d0dc2765b4f2df6eddd09380938e52b351
mrp3dro22 <140920961+mrp3dro22@users.noreply.github.com>
2023-10-04 20:51:31 +0100

    Add files via upload

---
+++ Libft/ft_calloc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void    *ft_calloc(size_t nitems, size_t size)
+{
+    void *ptr;
+    unsigned int total_size = nitems * size;
+
+    ptr = malloc(total_size);
+    if(ptr != NULL)
+    {
+        memset(ptr, 0, total_size);
+    }
+    return (ptr);
+}
+
+/*
+int main() {
+    int* int_ptr = (int*)ft_calloc(15, sizeof(int));
+
+    if (int_ptr != NULL) {
+        for (int i = 0; i < 10; i++) {
+            printf("%d ", int_ptr[i]);
+        }
+        free(int_ptr);
+    } else {
+        printf("Falha na alocação de memória.\n");
+    }
+
+    return 0;
+}
+*/
