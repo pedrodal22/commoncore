3d76910c12cdd5b2d1240285db638d7c0aca9a75
mrp3dro22 <140920961+mrp3dro22@users.noreply.github.com>
2023-10-09 00:27:29 +0100

    Add files via upload

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -1,25 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pfranco- <pfranco-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 15:02:16 by pfranco-          #+#    #+#             */
+/*   Updated: 2023/10/05 16:14:10 by pfranco-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "libft.h"
 
-char	*ft_strtrim(char *s1, const char *set)
+char    *ft_strtrim(char *s1, const char *set)
 {
-	int	i;
-	int	p;
-	int	temp;
-	int size = strlen(set);
+    int    i;
+    int a;
+    int k;
+    char *final;
+    int size = ft_strlen(set);
+    int s_size = ft_strlen(s1);
 
-	i = 0;
-	while (i < size)
-	{
-		if (tab[i] > tab[p])
-		{
-			temp = tab[i];
-			tab[i] = tab[p];
-			tab[p] = temp;
-		}
-		p++;
-	}
+    i = 0;
+    a = 0;
+    k = 0;
+    while (k != 1)
+    {
+        while(s1[i] != set[a] && a < size)
+        {
+            a++;
+        }
+        if (s1[i] == set[a])
+        {
+            s1[i] = 'A';
+            i++;
+            a = 0;
+        }
+        else
+        {
+            k++;
+        }
+    }
+    a = 0;
+    k = 0;
+    while (k != 1)
+    {
+        while(s1[s_size - 1] != set[a] && a < size)
+        {
+            a++;
+        }
+        if (s1[s_size - 1] == set[a])
+        {
+            s1[s_size - 1] = 'A';
+            s_size--;
+            a = 0;
+        }
+        else
+        {
+            k++;
+        }
+    }
+
+    i = 0;
+    a = 0;
+    int h;
+
+    while (s1[i] == 'A')
+    {
+        i++;
+        h = i;
+    }
+    while (s1[i] != 'A')
+    {
+        a++;
+        i++;
+    }
+    final = malloc(a + 1);
+    if (final == NULL)
+    {
+        return NULL;
+    }
+    a = 0;
+    while (s1[h] != 'A')
+    {
+        final[a] = s1[h];
+        a++;
+        h++;
+    }
+    final[a] = '\0';
+    return (final);
+}
+
+int main() {
+    char input[] = "  ab acHello, abc World! acb ca";
+    const char set[] = "a bc";
+    char *result = ft_strtrim(input, set);
+
+    printf("Result: '%s'\n", result);
+    return 0;
 }
 
 /*
@@ -52,22 +132,5 @@ char	*ft_strtrim(char *s1, const char *set)
 	}
 	final[i] = '\0';
 	return(final);
-}
-
-
-int main() {
-    char str[] = "ghrOlá,kk Mugndo!grsdgg";
-    char set[] = "ghr";
-
-    char *resultado = ft_strtrim(str, set);
-
-    if (resultado != NULL) {
-        printf("String resultante: '%s'\n", resultado);
-        free(resultado); // Não se esqueça de liberar a memória alocada
-    } else {
-        printf("Erro na alocação de memória.\n");
-    }
-
-    return 0;
 }
 */
