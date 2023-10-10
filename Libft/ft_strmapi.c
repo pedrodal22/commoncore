8b635c51dd86e311f537732ff021416591174d6d
mrp3dro22 <140920961+mrp3dro22@users.noreply.github.com>
2023-10-10 15:25:25 +0100

    Add files via upload

---
+++ Libft/ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 15:20:16 by pedro             #+#    #+#             */
+/*   Updated: 2023/10/10 15:23:52 by pedro            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*final;
+	int		i;
+	int		size;
+
+	size = ft_strlen(s);
+	i = 0;
+	final = malloc(size + 1);
+	if (final == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		final[i] = f(i, s[i]);
+		i++;
+	}
+	final[i] = '\0';
+	return (final);
+}
