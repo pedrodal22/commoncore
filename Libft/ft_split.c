f7966d8543df84210cf041a42c8055f87bec0e24
mrp3dro22 <140920961+mrp3dro22@users.noreply.github.com>
2023-10-14 18:21:37 +0100

    Add files via upload

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -3,45 +3,148 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pfranco- <pfranco-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 13:40:58 by pfranco-          #+#    #+#             */
-/*   Updated: 2023/10/06 14:09:53 by pfranco-         ###   ########.fr       */
+/*   Updated: 2023/10/14 18:19:31 by pedro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 #include "libft.h"
 
-static int count_word(char const *s)
+/*
+char **ft_split(char const *s, char c)
+{
+    int i, start = 0;
+    char **result;
+    int size = ft_strlen(s) + 1;
+    result = (char **)malloc(size);
+
+    while(s[i] != '\0')
+    {
+        if(s[i] == c)
+        {
+            result[start] = ft_substr(s, start, i - start);
+            start = i + 1;
+        }
+        else 
+        {
+            *result[i] = s[i];
+        i++;
+        }   
+    }
+    return (result);
+}
+
+int main ()
+{
+    char string[] = "Ola a todos hoje";
+    char split = ' ';
+    char *result = *ft_split(string, split);
+    printf("%s", result);
+}
+*/
+
+
+static int nbr_of_strs(char const *s, char c)
 {
     int i;
-    i = 0;
+    int a;
     
+    i = 0;
+    a = 0;
     while (s[i] != '\0')
     {
+        if (s[i] == c)
+        {
+            a++;
+        }
         i++;
     }
-    return(i++);
+    return(a);
+}
+
+
+int word_count(const char *s, char c)
+{
+    int i;
+    
+    i = 0;
+    while (s[i] != '\0')
+    {
+        while (s[i] == c)
+        {
+            i++;
+        }
+         if (s[i] != '\0') 
+        {
+            while (s[i] != c && s[i] != '\0')
+            {
+                i++;
+            }
+        }
+    }
+    return (i);
 }
 
 char **ft_split(char const *s, char c)
 {
+    char **final;
     int i;
-    int sep_count;
-    char **str;
-
+    int k;
+    int a;
+    int size;
+    int mem;
+    
     i = 0;
-    sep_count = 0;
-    while (s[i++] != '\0')
+    a = 0;
+    mem = nbr_of_strs(s, c);
+    k = 0;
+    size = ft_strlen(s);
+    final = (char **)malloc(mem + 1);
+    while (k < mem)
     {
-        if (s[i] == c)
+        final[k] = (char *)malloc(mem);
+        k++;
+    }
+    
+    i = 0;
+    while (s[i] != '\0')
+    {
+        a = 0;
+        while (s[i] != c && s[i] != '\0')
         {
-            sep_count++;
+            final[k][a] = s[i];
+            a++;
+            i++;
         }
+        final[k][a] = '\0'; 
+        k++;
+        
+        if (s[i] == c)
+            i++;
+    }
+    final[k] = NULL;
+    
+    return (final);
+}
+
+int main() {
+    char const *s = "Esta e uma frase de exemplo";
+    char c = ' ';
+    char **result = ft_split(s, c);
+
+    if (result) {
+        for (int i = 0; result[i] != NULL; i++) {
+            printf("%s\n", result[i]);
+            free(result[i]);
+        }
+
+        free(result);
+    } else {
+        printf("A alocação de memória falhou.\n");
     }
 
-    
+    return 0;
 }
