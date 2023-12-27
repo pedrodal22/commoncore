b8eaf8112cff6d4c6f1a7e72c9133bef573be020
mrp3dro22 <140920961+mrp3dro22@users.noreply.github.com>
2023-12-27 09:22:26 +0000

    Update get_next_line_utils.c

---
+++ gnl/get_next_line_utils.c
@@ -0,0 +1,19 @@
+#include "get_next_line.h"
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	a;
+
+	a = 0;
+	while (src[a] != '\0' && a < n)
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+	return (dest);
+}
