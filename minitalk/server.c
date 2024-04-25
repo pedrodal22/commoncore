41f15b518c4d601ff5e597ecb75d7cfe912d6c75
Pedro Almeida <140920961+pedrodal22@users.noreply.github.com>
2024-04-25 16:46:15 +0100

    Add files via upload

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,64 +6,52 @@
 /*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 17:19:07 by pedro             #+#    #+#             */
-/*   Updated: 2024/04/25 12:52:54 by pedro            ###   ########.fr       */
+/*   Updated: 2024/04/25 17:29:19 by pedro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-char *store_bit_value(int signal)
+void	show_chr(int signal)
+{
+	char		chr;
+	static int	bit_index;
+
+	if (signal == SIGUSR1)
+		chr += (1 << bit_index);
+	if (bit_index++ == 7)
+	{
+		write(1, &chr, 1);
+		bit_index = 0;
+		chr = 0;
+	}
+}
+
+int	main(void)
+{
+	int	pid;
+
+	pid = getpid();
+	ft_printf("\t PID: %d\n", pid);
+	signal(SIGUSR1, show_chr);
+	signal(SIGUSR2, show_chr);
+	while (1)
+		sleep(1);
+}
+
+/* char *store_bit_value(int signal)
 {
    char *bit_values = (char *)malloc(sizeof(char) * 8);
-   int i;
+   static int i;
 
    i = 0;
 	while(i >= 0)
-   {
+	{
          if (signal == SIGUSR1)
             bit_values[i] = (char)49;
          else
             bit_values[i] = (char)48;
-         i--;
-   }
-   bit_values[8] = '\0';
+        i--;
+	}
    return(bit_values);
-}
-
-void show_chr(int signal)
-{  
-    char *bit_values;
-    char chr;
-    static int i;
-    static int bit_index;
-   
-   bit_values = store_bit_value(signal);
-   i = 7;
-   while (i >= 0)
-   {
-      if (bit_values[i] == 49 || bit_values[i] == 48)
-      {
-         if (bit_values[i] == 49)
-            chr += (1 << bit_index);
-         if (bit_index++ == 7)
-         {
-            write(1, &chr, 1);
-            bit_index = 0;
-            chr = 0;
-         }
-      }
-      i--;
-   }
-}
-
-int main (void)
-{
-   int pid;
-   
-   pid = getpid();
-   ft_printf("\t PID: %d\n", pid);
-   signal(SIGUSR1, show_chr);
-   signal(SIGUSR2, show_chr);
-   while(1)
-      sleep(1);
-} 
+} */
